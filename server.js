54e04530afc4c8090176664a6cd40bd0f82d4fd1
MJavad Hasanzade <hjavad522@gmail.com>
2022-04-13 21:21:20 +0430

    Initial Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+
+//! type of data
+//! naun
+//! adj
+
+app.get("/?q=3", (req, res) => {
+  //? 1
+  res.send({ name1: "Soft", name2: "warm", name3: "Kitty" });
+});
+
+app.get("/noun", (req, res) => {
+  //? 2
+  res.send({ name1: "Soft", name2: "warm", name3: "Kitty" });
+});
+
+app.post("/noun", (req, res) => {
+  //? 3
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.put("/noun", (req, res) => {
+  //? 4
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.delete("/noun", (req, res) => {
+  //? 5
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.get("/adj", (req, res) => {
+  //? 6
+  res.send({ name1: "Soft", name2: "warm", name3: "Kitty" });
+});
+
+app.post("/adj", (req, res) => {
+  //? 7
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.put("/adj", (req, res) => {
+  //? 8
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.delete("/adj", (req, res) => {
+  //? 9
+  res.send({ name1: "Soft", name2: "Kitty" });
+});
+
+app.listen(3000, () => {
+  console.log("server is listeing to port 3000");
+});
