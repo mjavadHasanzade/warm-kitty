54d98b08775b8258c6ca670fa75714de1cfc831f
MJavad Hasanzade <hjavad522@gmail.com>
2022-04-17 20:41:01 +0430

    feat: add default controller and use random name selection

---
+++ Controllers/default.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const getAll = (req, res) => {
+  //? 1
+  const nouns = ["chockolate", "glass", "Cat", "Pen", "pillow"];
+  const adj = ["warm", "scary", "Blue", "funny", "salty"];
+
+  res.send({
+    name1: adj[Math.ceil(Math.random() * adj.length - 1)],
+    name2: nouns[Math.ceil(Math.random() * nouns.length - 1)],
+  });
+};
+
+module.exports = { getAll };
