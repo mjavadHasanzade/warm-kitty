d0287815d10ef331abe22bd81368042a1bdc610a
Mjavad Hasanzade <hjavad522@gmail.com>
2022-09-18 16:20:18 +0430

    feat: workflow

--- Controllers/default.js
+++ Controllers/default.js
@@ -4,6 +4,7 @@ const getAll = (req, res) => {
   const adj = ["warm", "scary", "Blue", "funny", "salty"];
 
   res.send({
+    name0: process.env.mjavad,
     name1: adj[Math.ceil(Math.random() * adj.length - 1)],
     name2: nouns[Math.ceil(Math.random() * nouns.length - 1)],
   });
