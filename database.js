57d539d30bce345974729ff52a891419cd0f256e
mjavadHasanzade <hjavad522@gmail.com>
2024-04-17 12:07:29 +0330

    feat: use liara instead of fandogh

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("warm-kitty", "user", "pass", {
   dialect: "sqlite",
-  host: "./dev.sqlite",
+  host: "./resources/dev.sqlite",
 });
 
 module.exports = sequelize;
