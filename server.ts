a32e0f112a2825181f8e480e202b5e4f186a5816
mjavadHasanzade <hjavad522@gmail.com>
2024-04-22 12:47:53 +0330

    feat: log user data

--- server.ts
+++ server.ts
@@ -8,7 +8,9 @@ import adjectiveRoutes from "./Routes/adjective";
 import qaRoutes from "./Routes/fakeQA";
 import FakeQA from "./Models/fakeQA";
 import { nouns, adjs, qas } from "./mock";
-import { Request, Response } from "express";
+import { Request, Response, ErrorRequestHandler } from "express";
+import { DatabaseError, ValidationError } from "./errors";
+import { lookup } from "geoip-lite";
 
 declare module "express-serve-static-core" {
   interface Request {
@@ -20,7 +22,7 @@ const app = express();
 
 //? initialize the database
 sequelize
-  .sync({ force: true })
+  .sync({})
   .then(async () => {
     await Nouns.bulkCreate(nouns);
     await Adjectives.bulkCreate(adjs);
@@ -32,21 +34,51 @@ sequelize
 
 app.use(express.json());
 
-
-
-const middleware = (req: Request, res: Response, next: express.NextFunction) => {
+const langHandler = (req: Request, res: Response, next: express.NextFunction) => {
   if (req.query.lang == "en" || req.query.lang == "fa")
     req.language = req.query.lang;
   else
     req.language = "fa"
+  // throw new ValidationError("custom error")
   next();
 }
 
-app.use("*", middleware);
+const logger = (req: Request, res: Response, next: express.NextFunction) => {
+  const oldSend = res.send;
+  res.send = function (body: any) {
+    console.log('Headers: ' + JSON.stringify(req.headers));
+    console.log('IP: ' + JSON.stringify(req.ip));
+
+    var geo = lookup(req.ip);
+
+    console.log("Browser: " + req.headers["user-agent"]);
+    console.log("Language: " + req.headers["accept-language"]);
+    console.log("Country: " + (geo ? geo.country : "Unknown"));
+    console.log("Region: " + (geo ? geo.region : "Unknown"));
+
+    console.log(geo);
+    return oldSend.call(res, body);
+  };
+
+  next();
+}
+
+app.use("*", langHandler, logger);
 app.use("/", defaultRoutes);
 app.use("/nouns", nounsRoutes);
 app.use("/adjs", adjectiveRoutes);
 app.use("/qas", qaRoutes);
+
+const errorHandler: ErrorRequestHandler = (err, request, res, next) => {
+  if (err instanceof DatabaseError || err instanceof ValidationError) {
+    res.status(err.statusCode).send({ error: err.message, type: err.name });
+  } else {
+    console.error(err);
+    res.status(500).send('An error occurred!');
+  }
+}
+
+app.use(errorHandler);
 
 
 app.listen(3000, () => {
